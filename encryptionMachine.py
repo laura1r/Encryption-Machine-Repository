b4977e3fb4e8d6ae29a287945bd5f21405da177a
laura1r <lau.nrm3@gmail.com>
2025-04-07 08:07:54 -0400

    commit 2

--- encryptionMachine.py
+++ encryptionMachine.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import flet as ft
+
+def main(page: ft.Page):
+    pass
+
+    text = ft.Text()
+    drop = ft.Dropdown(label= "languages",
+                       options=[
+                           ft.dropdown.Option("Morse"),
+                           ft.dropdown.Option("Alien"),
+                           ft.dropdown.Option("Numbers"),
+                           ft.dropdown.Option("Decrypt")
+        ])
+    
+    #bEncrypt = ft.ElevatedButton(text="Encrypt")
+    #bDecrypt = ft.ElevatedButton(text="Decrypt")
+
+    page.add()
+ft.app(target=main)
