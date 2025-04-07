6e24982a6ae6ac0f284773a5eb08d3f75a8efd22
laura1r <lau.nrm3@gmail.com>
2025-04-07 08:19:41 -0400

    commit 4

--- encryptionMachine.py
+++ encryptionMachine.py
@@ -3,17 +3,21 @@ import flet as ft
 def main(page: ft.Page):
     pass
 
-    text = ft.Text()
+    text1 = ft.Text("Shut up Calcium")
+    text2 = ft.Text("I don't know my name")
+    text3 = ft.Text("Its all the same water")
+
+    image1 = ft.Image(src="/Users/laurarosello/Desktop/Image 7-4-25 at 8.14 AM (1).jpg")
+    image2 = ft.Image(src="/Users/laurarosello/Desktop/Image 7-4-25 at 8.14 AM.jpg")
+    image3 = ft.Image(src="/Users/laurarosello/Desktop/Image 7-4-25 at 8.13 AM.jpg")
+
     drop = ft.Dropdown(label= "languages",
                        options=[
-                           ft.dropdown.Option("Morse"),
-                           ft.dropdown.Option("Alien"),
-                           ft.dropdown.Option("Numbers"),
+                           ft.dropdown.Option("Chinese"),
+                           ft.dropdown.Option("Japanese"),
+                           ft.dropdown.Option("French"),
                            ft.dropdown.Option("Decrypt")
         ])
-    
-    #bEncrypt = ft.ElevatedButton(text="Encrypt")
-    #bDecrypt = ft.ElevatedButton(text="Decrypt")
 
     page.add()
 ft.app(target=main)
