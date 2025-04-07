2aa334c441f19e51d1de296c8883d036e5e99c87
laura1r <lau.nrm3@gmail.com>
2025-04-07 14:42:24 -0400

    added image asset and assets folder

--- encryptionMachine.py
+++ encryptionMachine.py
@@ -2,20 +2,20 @@ import flet as ft
 
 def main(page: ft.Page):
     def buttom(e):
-        t.value = f"{e.control.value}"
+        t.value = f"The quote has been translated {e.control.value}"
         page.update()
 
     t = ft.Text()
 
     text1 = ft.Text("Shut up Calcium")
-    image1 = ft.Image(src="/Users/laurarosello/Desktop/Image 7-4-25 at 8.14 AM (1).jpg")
+    image1 = ft.Image(src="Image 7-4-25 at 8.14 AM (1).jpg")
 
     change = ft.RadioGroup(content=ft.Column([
         ft.Radio(value="Tais-toi Calcium", label="French"),
         ft.Radio(value="黙れカルシウム", label="Yapanese")
     ]), on_change=buttom)
 
-    row1 = ft.Row(text1)
-    row2 = ft.Row(image1)
+    row1 = ft.Row(controls=[text1])
+    row2 = ft.Row(controls=[image1])
     page.add(t, row1, row2, change)
-ft.app(target=main)
+ft.app(target=main, assets_dir="assets")
