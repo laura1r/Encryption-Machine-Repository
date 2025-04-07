86545bdbe442f986eccb616f97a6dfd44ab598d0
laura1r <lau.nrm3@gmail.com>
2025-04-07 08:36:51 -0400

    commit 7

--- encryptionMachine.py
+++ encryptionMachine.py
@@ -3,17 +3,16 @@ import flet as ft
 def main(page: ft.Page):
     pass
 
-    def images():
-        pass
+    t = ft.Text("Quote")
 
     text1 = ft.Text("Shut up Calcium")
     image1 = ft.Image(src="/Users/laurarosello/Desktop/Image 7-4-25 at 8.14 AM (1).jpg")
 
     change = ft.RadioGroup(content=ft.Column([
-        ft.Radio(value="French", label="French"),
-        ft.Radio(value="Yapanese", label="Yapanese")
+        ft.Radio(value="Tais-toi Calcium", label="French"),
+        ft.Radio(value="黙れカルシウム", label="Yapanese")
     ]))
 
     row1 = ft.Row(text1)
     row2 = ft.Row(image1)
-    page.add(row1, row2, change)
+    page.add(t, row1, row2, change)
