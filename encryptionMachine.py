3da6791bcf05df46f2fe76e449773565f36046b6
laura1r <lau.nrm3@gmail.com>
2025-04-07 08:28:41 -0400

    commit 5

--- encryptionMachine.py
+++ encryptionMachine.py
@@ -3,21 +3,17 @@ import flet as ft
 def main(page: ft.Page):
     pass
 
+    def images():
+        pass
+
     text1 = ft.Text("Shut up Calcium")
-    text2 = ft.Text("I don't know my name")
-    text3 = ft.Text("Its all the same water")
-
     image1 = ft.Image(src="/Users/laurarosello/Desktop/Image 7-4-25 at 8.14 AM (1).jpg")
-    image2 = ft.Image(src="/Users/laurarosello/Desktop/Image 7-4-25 at 8.14 AM.jpg")
-    image3 = ft.Image(src="/Users/laurarosello/Desktop/Image 7-4-25 at 8.13 AM.jpg")
 
-    drop = ft.Dropdown(label= "languages",
-                       options=[
-                           ft.dropdown.Option("Chinese"),
-                           ft.dropdown.Option("Japanese"),
-                           ft.dropdown.Option("French"),
-                           ft.dropdown.Option("Decrypt")
-        ])
+    change = ft.RadioGroup(content=ft.Column([
+        ft.Radio(value="French", label="French"),
+        ft.Radio(value="Yapanese", label="Yapanese")
+    ]))
 
-    page.add()
-ft.app(target=main)
+    row1 = ft.Row(text1)
+    row2 = ft.Row(image1)
+    page.add(row1, row2, change)
